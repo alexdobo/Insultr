a0fb5f07669dcc64ef9f2f6bf01b3452171ac68e
alexdobo <alexdobo@users.noreply.github.com>
2018-01-08 19:27:55 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,116 @@
+import praw
+import boto3
+
+
+
+def getInsult():
+    reddit = praw.Reddit(client_id='',client_secret='',user_agent='Alexa insult me bot')
+
+    foundOne = False
+    insultsSub = reddit.subreddit('insults')
+
+    while foundOne == False:
+        randomInsult = insultsSub.random()
+        if randomInsult.selftext == '':
+            foundOne = True
+            return(randomInsult.title)
+
+def sameResponseForEverything():
+    session_attributes = {}
+    speech_output = getInsult()
+    should_end_session = True
+    return build_response(session_attributes,build_speechlet_response('Put me in my place',speech_output,None,should_end_session))
+
+
+#pregenerated code:
+
+def on_session_started(session_started_request, session):
+    """ Called when the session starts """
+
+    print("on_session_started requestId=" + session_started_request['requestId']
+          + ", sessionId=" + session['sessionId'])
+
+def on_launch(launch_request, session):
+    """ Called when the user launches the skill without specifying what they
+    want
+    """
+
+    print("on_launch requestId=" + launch_request['requestId'] +
+          ", sessionId=" + session['sessionId'] + ", userId=" + session['user']['userId'])
+    # Dispatch to your skill's launch
+    return sameResponseForEverything()
+
+def on_intent(intent_request, session):
+    """ Called when the user specifies an intent for this skill """
+
+    print("on_intent requestId=" + intent_request['requestId'] +
+          ", sessionId=" + session['sessionId'] + ", userId="+session['user']['userId'])
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    # Dispatch to your skill's intent handlers
+    if intent_name == "AMAZON.HelpIntent":
+        return get_welcome_response()
+    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
+    else:
+        return sameResponseForEverything()
+
+def on_session_ended(session_ended_request, session):
+    """ Called when the user ends the session.
+
+    Is not called when the skill returns should_end_session=true
+    """
+    print("on_session_ended requestId=" + session_ended_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+#this is run when code is launched
+def handler(event, context):
+    print("event.session.application.applicationId=" + event['session']['application']['applicationId'])
+    """
+    Uncomment this if statement and populate with your skill's application ID to
+    prevent someone else from configuring a skill that sends requests to this
+    function.
+    """
+    # if (event['session']['application']['applicationId'] !=
+    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
+    #     raise ValueError("Invalid Application ID")
+
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
+
+
+# --------------- Helpers that build all of the responses ----------------------
+
+def build_speechlet_response(title, output, reprompt_text, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': title,
+            'content': output
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
